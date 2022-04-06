e88a1ffbc592693fb95778a0ccacd8f832253045
Martina Tricarico <tricaricomartina4@gmail.com>
2022-04-06 18:13:47 -0300

    part 3

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Carousel from "../Carousel";
+import { dark } from "../../styles/Themes";
+import Button from "../Button";
+import { ThemeProvider } from "styled-components";
 
 const Section = styled.section`
   min-height: 100vh;
   width: 100%;
   background-color: ${(props) => props.theme.text};
-  color: ${(props) => props.theme.body};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -32,6 +34,39 @@ const Box = styled.div`
   align-items: center;
 `;
 
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const SubText = styled.p`
+  font-size: ${(props) => props.theme.fontlg};
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+  margin: 1rem auto;
+  font-weight: 400;
+`;
+
+const SubTextLight = styled.p`
+  font-size: ${(props) => props.theme.fontmd};
+  color: ${(props) => `rgba(${props.theme.bodyRgba}, 0.6)`};
+  align-self: flex-start;
+  width: 80%;
+  margin: 1rem auto;
+  font-weight: 400;
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  margin: 1rem auto;
+  align-self: flex-start;
+`;
+
 const About = () => {
   return (
     <Section>
@@ -39,7 +74,25 @@ const About = () => {
         <Box>
           <Carousel />
         </Box>
-        <Box>Text</Box>
+        <Box>
+          <Title>Welcome To The Weirdos Club.</Title>
+          <SubText>
+            The WEIRDOS CLUB is a private collection of NFTs—unique digital
+            collectibles. The Weirdos are stored as ERC-721 tokens on the
+            Ethereum blockchain and hosted on IPFS.
+          </SubText>
+          <SubTextLight>
+            With more than 200+ hand drawn traits, each NFT is unique and comes
+            with a membership to an exclusive group of successful investors.
+            Join an ambitious ever-growing community with multiple benefits and
+            utilities.
+          </SubTextLight>
+          <ButtonContainer>
+            <ThemeProvider theme={dark}>
+              <Button text="JOIN OUR DISCORD" link="#"></Button>
+            </ThemeProvider>
+          </ButtonContainer>
+        </Box>
       </Container>
     </Section>
   );

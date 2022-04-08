378330cf3842d64841f13ee47a6412af3dce37b1
Martina Tricarico <tricaricomartina4@gmail.com>
2022-04-08 19:17:11 -0300

    part 4

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -1,7 +1,79 @@
 import React from "react";
+import styled from "styled-components";
+import DrawSvg from "../DrawSvg";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+`;
+
+const Container = styled.div`
+  width: 70%;
+  height: 200vh;
+  background-color: ${(props) => props.theme.body};
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Items = styled.ul`
+  list-style: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lightblue;
+`;
+
+const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
 
 const Roadmap = () => {
-  return <div>Roadmap</div>;
+  return (
+    <Section>
+      <Title>Roadmap</Title>
+      <Container>
+        <SvgContainer>
+          <DrawSvg />
+        </SvgContainer>
+        <Items>
+          <Item>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+          <Item>4</Item>
+          <Item>5</Item>
+          <Item>6</Item>
+        </Items>
+      </Container>
+    </Section>
+  );
 };
 
 export default Roadmap;

8a1038ecd57f2da1727edd49288b3039d2ff3abf
Martina Tricarico <tricaricomartina4@gmail.com>
2022-04-12 16:33:31 -0300

    part 7

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -1,5 +1,8 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useLayoutEffect, useRef } from "react";
+import { ScrollTrigger } from "react-gsap";
 import styled from "styled-components";
+import Accordion from "../Accordion";
 
 const Section = styled.section`
   min-height: 100vh;
@@ -38,12 +41,63 @@ const Box = styled.div`
 `;
 
 const Faq = () => {
+  const ref = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+  useLayoutEffect(() => {
+    let element = ref.current;
+    ScrollTrigger.create({
+      trigger: element,
+      start: "top top",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false,
+      scrub: true,
+    });
+    return () => {}, [];
+  });
   return (
     <Section>
       <Title>Faq</Title>
       <Container>
-        <Box>col 1</Box>
-        <Box>col 2</Box>
+        <Box>
+          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+            Once minted or bought simply connect to your OpenSea account to view
+            your NFTs.
+          </Accordion>
+          <Accordion title="WHAT IS THE METAVERSE?">
+            A metaverse is a network of 3D virtual worlds focused on social
+            connection. In futurism and science fiction, it is often described
+            as a hypothetical iteration of the Internet as a single, universal
+            virtual world that is facilitated by the use of virtual and
+            augmented reality headsets.
+          </Accordion>
+          <Accordion title="WHY DO WE NEED ROYALTIES?">
+            The amount of royalties was fixed at 5% to finance the Weirdos
+            Club's projects. We have the ambition to organize multiple events
+            around the world in order to strengthen the community and build a
+            network of entrepreneurs and investors with the same mindset and
+            common interests.
+          </Accordion>
+        </Box>
+        <Box>
+          <Accordion title="HOW CAN I USE MY NFT?">
+            You will be able to use your NFT as an avatar in the Metaverse and
+            our future video game. Holding also means that you are part of an
+            exclusive network of investors and entrepreneurs.
+          </Accordion>
+          <Accordion title="WHAT ARE THE WEIRDOS?">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            deserunt consequatur quisquam maxime molestias dolores ipsum,
+            exercitationem vel sint quidem aliquam modi quis impedit corporis
+            unde inventore fugiat provident in.
+          </Accordion>
+          <Accordion title="WHAT IS THE FUSION PROCESS?">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel enim
+            veritatis iusto officia. Exercitationem, ducimus reiciendis. Rem,
+            maxime, similique neque minus aliquam dolore doloremque laboriosam,
+            facilis quibusdam unde sint officia.
+          </Accordion>
+        </Box>
       </Container>
     </Section>
   );
